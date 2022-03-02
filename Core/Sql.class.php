d7a4b73ffc20086e696253953b7754b3dff01f6d
Hoai-Viet Luc <hoaivietdoan@gmail.com>
2022-03-02 19:28:38 +0100

    Add singleton feature and fix database column

--- Core/Sql.class.php
+++ Core/Sql.class.php
@@ -4,78 +4,75 @@ namespace App\Core;
 
 abstract class Sql
 {
-    private $pdo;
-    private $table;
+	private $pdoInstance;
+	private $table;
 
-    public function __construct()
-    {
-        //Se connecter à la bdd
-        //il faudra mettre en place le singleton
-        try{
-            $this->pdo = new \PDO( DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME
-                ,DBUSER, DBPWD , [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]);
-        }catch (\Exception $e){
-            die("Erreur SQL : ".$e->getMessage());
-        }
+	public function __construct()
+	{
+		$this->pdoInstance = PDO::getIntance();
+		
+		//Si l'id n'est pas null alors on fait un update sinon on fait un insert
+		$calledClassExploded = explode("\\",get_called_class());
+		$this->table = DBPREFIXE.end($calledClassExploded);
+	}
 
-        //Si l'id n'est pas null alors on fait un update sinon on fait un insert
-        $calledClassExploded = explode("\\",get_called_class());
-        $this->table = DBPREFIXE.end($calledClassExploded);
+	/**
+	 * @param int $id
+	 */
+	public function setId(?int $id): object
+	{
+		$sql = "SELECT * FROM ".$this->table." WHERE id = ".$id;
+		$query = $this->pdoInstance->getPDO()->query($sql);
+		return $query->fetchObject(get_called_class());
+	}
 
-    }
+	public function save()
+	{
+		$columns = get_object_vars($this);
+		$columns = array_diff_key($columns, get_class_vars(get_class()));
 
-    /**
-     * @param int $id
-     */
-    public function setId(?int $id): object
-    {
-        $sql = "SELECT * FROM ".$this->table." WHERE idUser = ".$id;
-        $query = $this->pdo->query($sql);
-        return $query->fetchObject(get_called_class());
-    }
+		if($this->getId() == null){
+			$sql = "INSERT INTO ".$this->table." (".implode(",",array_keys($columns)).") VALUES ( :".implode(",:",array_keys($columns)).")";
+		}else{
+			$update = [];
+			foreach ($columns as $column=>$value)
+				$update[] = $column." = :".$column;
 
-    public function save()
-    {
-        $columns = get_object_vars($this);
-        $columns = array_diff_key($columns, get_class_vars(get_class()));
+			$sql = "UPDATE ".$this->table." SET ".implode(",",$update)." WHERE id = ".$this->getId();
+		}
 
-        if($this->getId() == null){
-            $sql = "INSERT INTO ".$this->table." (".implode(",",array_keys($columns)).") VALUES ( :".implode(",:",array_keys($columns)).")";
-        }else{
-            $update = [];
-            foreach ($columns as $column=>$value)
-                $update[] = $column." = :".$column;
-          
-            $sql = "UPDATE ".$this->table." SET ".implode(",",$update)." WHERE idUser = ".$this->getId() ;
+		echo "<pre>";
+		var_dump($this->table);
+		var_dump($sql);
+		var_dump($columns);
+		echo "</pre>";
 
-        }
+		$queryPrepared = $this->pdoInstance->getPDO()->prepare($sql);
+		$queryPrepared->execute( $columns );
+	}
 
-        $queryPrepared = $this->pdo->prepare($sql);
-        $queryPrepared->execute( $columns );
-    }
-	
-    public function delete()
-    {
-        $sql = "DELETE FROM " . $this->table . " WHERE idUser=" . $this->getId();
-        $queryPrepared = $this->pdo->query($sql);
-    }
+	public function delete()
+	{
+		$sql = "DELETE FROM " . $this->table . " WHERE id=" . $this->getId();
+		$queryPrepared = $this->pdoInstance->getPDO()->query($sql);
+	}
 
-    public function select(array $values,array $params)
-    {
-        $calledClassExploded = explode("\\",get_called_class());
-        $table = strtolower(DBPREFIXE.end($calledClassExploded));
+	public function select(array $values,array $params)
+	{
+		$calledClassExploded = explode("\\",get_called_class());
+		$table = strtolower(DBPREFIXE.end($calledClassExploded));
 
-        $sql = "SELECT ".implode(", ", $values)." FROM ".$table." WHERE ";
+		$sql = "SELECT ".implode(", ", $values)." FROM ".$table." WHERE ";
 
-        foreach ($params as $key => $values) {
-            $sql .= $key." = :".$key." AND ";
-        }
+		foreach ($params as $key => $values) {
+			$sql .= $key." = :".$key." AND ";
+		}
 
-        $sql = substr($sql,0,-4);
+		$sql = substr($sql,0,-4);
 
-        $queryPrepared = $this->pdo->prepare($sql);
-        $queryPrepared->execute( $params );
+		$queryPrepared = $this->pdoInstance->getPDO()->prepare($sql);
+		$queryPrepared->execute( $params );
 
-        return $queryPrepared->fetchAll(\PDO::FETCH_ASSOC);
-    }
+		return $queryPrepared->fetchAll(\PDO::FETCH_ASSOC);
+	}
 }
--- conf.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("DBUSER", "root2");
-define("DBPWD", "password2");
-define("DBHOST", "database");
-define("DBNAME", "TAL_Project_BDD");
-define("DBPORT", "3306");
-define("DBDRIVER", "mysql");
-define("DBPREFIXE", "TALBDD_");
