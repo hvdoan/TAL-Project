b4991e12b3f0a162736f1b92af9abcce57e17623
Théo HERVÉ <theojherve@gmail.com>
2022-06-07 14:53:46 +0200

    :fix: Fix connection problem on front

--- Controller/Main.class.php
+++ Controller/Main.class.php
@@ -128,7 +128,7 @@ class Main {
 								}
 								$htmlContent .= "<div class='message " . (($message['idMessage'] != 0)? "answerMessage" : "") . "'>";
 									$htmlContent .= "<div class='messageParent'>";
-										if(isset($_SESSION['id']) ? $userMessage->getId() == $_SESSION["id"] : false){
+										if($isConnected){
 											$htmlContent .= "<a class='pointer' onclick='deleteMessageFront(" . $forumId . ", " . $message['id'] . ")'>";
 												$htmlContent .= "<svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>";
 													$htmlContent .= "<path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>";
@@ -188,7 +188,7 @@ class Main {
 									}
 									$htmlContent .= "<div class='message " . (($answer['idMessage'] != 0)? "answerMessage" : "") . "'>";
 										$htmlContent .= "<div class='messageParent'>";
-											if(isset($_SESSION['id']) ? $userAnswer->getId() == $_SESSION["id"] : false){
+											if($isConnected){
 												$htmlContent .= "<a class='pointer' onclick='deleteMessageFront(" . $forumId . ", " . $answer['id'] . ")'>";
 													$htmlContent .= "<svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>";
 														$htmlContent .= "<path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>";
