259b0846b9f6ec240d50e03e61832f0e513b3cf9
Alexandre Hardy <hardyalexandre2000@gmail.com>
2022-07-12 19:21:19 +0200

    save data

---
+++ SASS/JS/editionStyle.js
@@ -0,0 +1,11 @@
+function saveStyle() {
+    valueHeader = $('#pickerHeader').val();
+    valueBackground = $('#pickerBackground').val();
+    valueTitle = $('#pickerTitle').val();
+    valueText = $('#pickerText').val();
+
+    console.log(valueHeader);
+    console.log(valueBackground);
+    console.log(valueTitle);
+    console.log(valueText);
+}
