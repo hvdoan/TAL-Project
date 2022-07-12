10e9a5135e40bffa82d75b9110654e5b42162e83
Hoai-Viet Luc <hoaivietdoan@gmail.com>
2022-07-12 12:03:34 +0200

    Rebuild PHPMailer
    Remove oauth2 and now use basic SMTP authentification

--- View/validateAccount.view.php
+++ View/validateAccount.view.php
@@ -1,1 +1,6 @@
-<h1>Validation de mail</h1>
+<main id="validate-account">
+    <section>
+        <h1>Votre adresse mail à bien été validé.</h1>
+        <h2>Activation du compte avec succès.</h2>
+    </section>
+</main>
